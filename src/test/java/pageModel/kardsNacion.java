8c1aadbea87e176c691e579c83768a5de5fa7c90
elzevir01 <matiasarques47@gmail.com>
2026-04-28 20:43:52 -0300

    fix kardsMain xpath

--- src/test/java/pageModel/kardsNacion.java
+++ src/test/java/pageModel/kardsNacion.java
@@ -10,26 +10,28 @@ public class kardsNacion extends Base {
     private String urlNacion = "https://www.kards.com/es/decks/deck-builder";
 
     ///// ELEMENTOS WEB /////
-    /// nacion principal Estados Unidos ///  ////div[text()='Estados Unidos']
-    private By nacionPrincipal = By.xpath("/html/body/div[2]/div[1]/main/div/div/div[2]/div/div[2]/div[4]/div/div[2]/div[2]");
+    /// nacion principal Estados Unidos /// ////div[text()='Estados Unidos']
+    private By nacionPrincipal = By
+            .xpath("/html/body/div[2]/div[1]/main/div/div/div[2]/div/div[2]/div[4]/div/div[2]/div[2]");
     /// nacion aliada Finlandia ///
     private By nacionAliada = By.xpath("/html/body/div[2]/div[1]/main/div/div/div[2]/div/div[4]/div[9]");
     /// Boton Crear Mazo ///
     private By botonCrearMazo = By.xpath("/html/body/div[2]/div[1]/main/div/div/div[2]/div/div[5]/button");
 
-    
     ///// CONSTRUCTOR /////
     public kardsNacion(WebDriver driver) {
         super.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    
+
     ///// METODOS /////
     public void seleccionarNacion() {
-        esperarElemento(nacionPrincipal);
-        clickElemento(nacionPrincipal);
-        clickElemento(nacionAliada);
-        clickElemento(botonCrearMazo);
-        
+        if (driver.getCurrentUrl().equals(urlNacion)) {
+            esperarElemento(nacionPrincipal);
+            clickElemento(nacionPrincipal);
+            clickElemento(nacionAliada);
+            clickElemento(botonCrearMazo);
+        }
+
     }
 }
